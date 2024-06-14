d0b294b41c71ac911d8a4cf6d6b2865298b303f8
ytonykaku <juniormanhaesfilho@hotmail.com>
2024-06-14 12:49:31 -0300

    Starting Next project
    Former-commit-id: 156ecb87d112eabf5bfedd651e707be7ffd8ecf3

---
+++ next_project/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import navbar from './navbar';
+
+function Index(){
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <Navbar />
+    </nav>
+  );
+}
+
+export default Index;
